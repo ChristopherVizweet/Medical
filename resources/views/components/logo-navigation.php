751563dffb72b32772fd88619b9d41c3a22ddde6
ChristopherVizweet <cris.5abril2019@gmail.com>
2026-06-09 17:03:51 -0600

    Backup 9 de junio 2026: Fix: arreglar de nuevo direccion de logo

--- resources/views/components/logo-navigation.php
+++ resources/views/components/logo-navigation.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('img/logo_medical_logo.png') }}"   alt="Mi Logo" width="200" height="300">
+<img src="/img/logo_medical_logo.png" width="100" height="200" alt="Medical Logo" class="logo">
